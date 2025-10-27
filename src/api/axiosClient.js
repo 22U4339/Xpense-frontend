c74cdde8f1fcd175fff6aea65e10445460d48fe7
Moses Abana <22U4339@student.mau.edu.ng>
2025-10-27 19:51:27 +0100

    Update axios client base URL for production

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const client = axios.create({
-  baseURL: "https://xpense-latest.onrender.com",
+  baseURL: "https://xpense-production.up.railway.app",
   headers: { "Content-Type": "application/json" },
 });
 
